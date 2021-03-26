23ac618b24fbe7a621cbf273b3feeb175ed806fb
Andrew Jarrett <ahrjarrett@gmail.com>
2021-03-25 19:36:56 -0500

    move org to private

---
+++ packages/typelevel/src/typelevel.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.typelevel = void 0;
+function typelevel() {
+    return null;
+}
+exports.typelevel = typelevel;
+//# sourceMappingURL=typelevel.js.map
