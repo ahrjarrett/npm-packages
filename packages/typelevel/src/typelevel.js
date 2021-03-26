039e8930a8adb1ae627ce6d330321c4c6dac01d5
Andrew Jarrett <ahrjarrett@gmail.com>
2021-03-25 20:17:20 -0500

    0.1.2

--- packages/typelevel/src/typelevel.js
+++ packages/typelevel/src/typelevel.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.typelevel = void 0;
 function typelevel() {
+    console.log('requiring function `typelevel` from npm package @ahrjarrett-org/typelevel');
     return null;
 }
 exports.typelevel = typelevel;
